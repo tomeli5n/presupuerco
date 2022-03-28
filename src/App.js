be7ff1219e8c906ab650ccf700f794589e4a8e68
Mario Tomelin <tomelin5@gmail.com>
2022-03-27 21:13:35 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import { Button, Stack } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
 
 function App() {
   return (
-    "algo"
+    <container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto"> 🐖 Presupuerco</h1>
+        <Button variant="primary">Primary</Button>
+        <Button variant="outline-primary">Nueva</Button>
+      </Stack>
+      <div 
+        style={{ 
+          display: "grid", 
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+          gap: "1rem", 
+          alignItems: "flex-start",
+        }}>
+
+        <BudgetCard name="entretenimiento" gray amount={700} max={1000}></BudgetCard>
+
+        </div>
+    </container>
+    
   );
 }
 
