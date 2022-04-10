4e5c4483945cdb62f1143a7230313089b05e016a
Mario Tomelin <tomelin5@gmail.com>
2022-04-10 10:23:17 -0300

    useContext

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,4 +1,10 @@
+import React, {useContext} from 'react';
 
+const BudgetsContext = React.createContext();
+
+export function useBudgets() {
+    return useContext(BudgetsContext);
+}
 
 export const BudgetsProvider = ({ children }) => {
     return children
