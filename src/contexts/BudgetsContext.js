f5ca3ef6e2dd3f6296b7c465270039c0141a4026
Mario Tomelin <tomelin5@gmail.com>
2022-04-12 20:44:26 -0300

    LocalStorage + useState se crean los arrays de budgets y expenses vacios en Devtools - > localstorag

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,8 +1,10 @@
 import React, {useContext, useState} from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const BudgetsContext = React.createContext();
 
+let prevBudgets = [];
 export function useBudgets() {
     return useContext(BudgetsContext);
 }
@@ -21,8 +23,8 @@ export function useBudgets() {
 //     description:
 // }
 export const BudgetsProvider = ({ children }) => {
-    const [budgets, setBudgets] = useState([])
-    const [expenses, setExpenses] = useState([])
+    const [budgets, setBudgets] = useLocalStorage("budgets", [])
+    const [expenses, setExpenses] = useLocalStorage("expenses", [])
     function getBudgetExpenses(budgetId) {
         return expenses.filter(expense => expense.budgetId === budgetId)
     }
@@ -40,14 +42,15 @@ export const BudgetsProvider = ({ children }) => {
         })
     }
     function deleteBudget({ id }){
+        // TODO: deal with uncategorized expenses
         setBudgets(prevBudgets => {
             return prevBudgets.filter(budget => budget.id !== id)
-        }
+        })
     }
     function deleteExpense({ id }) {
         setExpenses(prevExpenses => {
             return prevExpenses.filter(expense => expense.id !== id)
-        }
+        })
     }
     return (
         <BudgetsContext.Provider value={{
