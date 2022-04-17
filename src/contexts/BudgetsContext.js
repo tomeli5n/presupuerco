970c86b4759b8ade85ced02e45ca9868aa860e75
Mario Tomelin <tomelin5@gmail.com>
2022-04-17 13:04:45 -0300

    uncategorize expenses on delete parent budget

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -31,11 +31,17 @@ export const BudgetsProvider = ({ children }) => {
         })
     }
     function deleteBudget({ id }){
-        // TODO: deal with uncategorized expenses
+        setExpenses(prevExpenses => {
+            return prevExpenses.map(expense => {
+                if (expense.budgetId !== id) return expense
+                return {...expense, budgetId: UNCATEGORIZED_BUDGET_ID}
+            })
+        })
         setBudgets(prevBudgets => {
             return prevBudgets.filter(budget => budget.id !== id)
         })
     }
+    
     function deleteExpense({ id }) {
         setExpenses(prevExpenses => {
             return prevExpenses.filter(expense => expense.id !== id)
