da87f2463016839b73d711fe5f89ffe9142a2b1b
Mario Tomelin <tomelin5@gmail.com>
2022-04-17 11:48:05 -0300

    fix: currencyFormatter

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -1,5 +1,6 @@
 import { Modal, Button, Stack } from "react-bootstrap";
 import { useBudgets, UNCATEGORIZED_BUDGET_ID } from "../contexts/BudgetsContext"
+import { currencyFormatter } from "../utils";
 
 export default function ViewExpensesModal({ budgetId, handleClose}) {
     const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets()
@@ -9,6 +10,7 @@ export default function ViewExpensesModal({ budgetId, handleClose}) {
             ? { name: "Uncategorized", id: UNCATEGORIZED_BUDGET_ID }
             : budgets.find( b => b.id === budgetId)
 
+    const expenses = getBudgetExpenses(budgetId)
     return (
     <Modal show={budgetId != null} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -25,7 +27,15 @@ export default function ViewExpensesModal({ budgetId, handleClose}) {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                
+                <Stack direction = "vertical" gap ="3">
+                    {expenses.map(expense => (
+                        <Stack direction="horizontal" gap="2" key={expense.id}>
+                            <div className = "me-auto fs-4">{expense.description}</div>
+                            <div className = "fs-5">{currencyFormatter.format(expense.amount)  }</div>
+                            <Button size="sm" variant="danger">&Times;</Button>
+                        </Stack>
+                    ))}
+                </Stack>
             </Modal.Body>
     </Modal>
     )
